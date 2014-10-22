8722655362e410b2e2b550ae300ebb12d44155a2
Mike Zevitas <mzevitas@gmail.com>
2014-10-22 19:31:07 -0400

    Design work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('The Iron Yard Rocks');
+var player= {
+  name: "Player 1",
+  health: 100,
+};
+
+var enemy = {
+  name: "Enemy",
+  health: 100
+};
+
+$(".name1").html( player.name );
+$(".name2").html( enemy.name );
