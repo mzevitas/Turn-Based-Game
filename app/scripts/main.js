9de2f372e41be0a594a616569b66fa13b5eb226b
Mike Zevitas <mzevitas@gmail.com>
2014-10-27 08:49:04 -0400

    monday heroku

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,50 +2,37 @@ var Admin = function (options) {
  options = options || {};
   this.name = options.name;
   this.health = 100;
+  this.type = options.type;
   this.avatar = options.avatar;
 
 
   this.attack = function (attacker) {
-    attacker.health = attacker.health - 10;
+    attacker.health = attacker.health - _.random(0, 10);
   };
   this.special = function (attacker) {
-    attacker.health = attacker.health -20;
+    attacker.health = attacker.health - _.random(7, 15);
   };
 };
 
 
 var Teacher = function (options){
-  var spec, att;
+  var img;
   options = options || {};
   this.name = options.name;
-  this.type = options.type;
+  this.avatar = options.avatar;
   this.health = 100;
-  switch (this.type) {
-    case "1":
-      att = 10;
-      spec = 20;
-    break;
-    case "2":
-      att = 15;
-      spec = 25;
-      break;
-    case "3":
-      att = 5;
-      spec = 30;
-    break;
-    default:
-      break;
 
-  }
+
   this.attack = function (attacker){
-  attacker.health = attacker.health - att;
+  attacker.health = attacker.health - _.random(0, 10);
 
   };
 
   this.special = function (attacker) {
-  attacker.health = attacker.health - spec;
+  attacker.health = attacker.health - _.random(7,15);
   };
 };
+
 
 
 
@@ -59,14 +46,17 @@ $('.fill button').on('click', function (event){
    player1 = new Teacher({
     name: $(this).text(),
     type: $(this).attr('name'),
-    avatar: $(this).html()
+    avatar: $(this).attr('img')
 
 
   });
 $('.fill' ).click(function() {
   $( this ).slideUp();
 
-  $('.name1').prepend(player1.name).find('.n1health').text(player1.health).find('.stageleft img').html(player1.avatar);
+  $('.name1').prepend(player1.name).find('.n1health').text(player1.health);
+
+$('.stageleft img').prepend(player1.avatar);
+
 
 });
 });
@@ -77,18 +67,21 @@ $('.fill2 button').on('click', function (event){
 
     player2 = new Admin({
      name: $(this).text(),
-     avatar: $(this).html()
-    //  type: $(this).attr('name'),
+     avatar: $(this).attr('img')
+
 });
 $('.fill2' ).click(function() {
   $( this ).slideUp();
   $('.name2').prepend(player2.name).find('.n2health').text(player2.health);
+
+$('.stageright img').prepend(player2.avatar);
+
 });
 
 $('.modalDialog').hide();
 
 
-
+// Fight Scene
 
 $('.attackbtn').on('click', function (event){
   event.preventDefault();
@@ -99,9 +92,12 @@ player1.attack(player2);
 $('.n2health').text(player2.health);
 
 player2.attack(player1);
-$('.n2health').text(player1.health);
+$('.n1health').text(player1.health);
 
-// if (player1.health <= 0 || player2.health <= 0){
+if (player1.health <= 0 || player2.health <= 0);
+
+
+
 
 
 
